f982d3a83bb23de5c584d65bdd9aef4909c9da4d
ItsTayeo1 <ProFernandezMF@gmail.com>
2025-01-10 11:37:29 -0800

    player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,7 +18,22 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        
+        if(Input.GetKey(KeyCode.W))
+        {
+            _playerTransform.position += Vector3.up * _speed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            _playerTransform.position += Vector3.down * _speed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            _playerTransform.position += Vector3.right * _speed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            _playerTransform.position += Vector3.left * _speed * Time.deltaTime;
+        }
     }
 
     public void PlantSeed ()
